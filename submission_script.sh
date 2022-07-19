d50cfe9505ccfa24bf2cdfdfad7fbb3dda5020f4
Neiman Golden <aneimangolden@ip-0A125246.rhxrlfvjyzbupc03cc22jkch3c.xx.internal.cloudapp.net>
2022-07-19 11:24:57 -0700

    script for pipeline test

---
+++ submission_script.sh
@@ -0,0 +1,9 @@
+# submission script for test of pipeline-call-SRC
+
+python3 /hot/software/package/tool-submit-nf/Python/release/2.2.0/submit_nextflow_pipeline.py \
+    --nextflow_script /hot/user/yashpatel/pipeline-call-SRC/pipeline-call-SRC/main.nf \
+    --nextflow_config /hot/software/pipeline/pipeline-call-SRC/Nextflow/development/unreleased/yashpatel-add-phylowgs/ILHNLNEV000001-N002-A01-F.config \
+    --nextflow_yaml /hot/software/pipeline/pipeline-call-SRC/Nextflow/development/unreleased/yashpatel-add-phylowgs/ILHNLNEV000001-N002-A01-F.yaml \
+    --pipeline_run_name ILHNLNEV000001-N002-A01-F \
+    --partition_type F16 \
+    --email ANeimanGolden@mednet.ucla.edu
