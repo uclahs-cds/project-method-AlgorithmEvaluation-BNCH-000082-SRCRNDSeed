01ffbb9ca82ff4943dee26d1b6037c65e698e927
Neiman Golden <aneimangolden@ip-0A125246.rhxrlfvjyzbupc03cc22jkch3c.xx.internal.cloudapp.net>
2022-08-03 09:34:14 -0700

    multisample strelka-battenberg-pyclone-vi

--- src/run-pipeline-call-src/ms_strelka2_battenberg_pyclone_submission_script.sh
+++ src/run-pipeline-call-src/ms_strelka2_battenberg_pyclone_submission_script.sh
@@ -2,20 +2,13 @@
 
 seeds=("51404" "366306" "423647" "838004" "50135" "628019" "97782" "253505" "659767" "13142")
 patients=(
-    "ILHNLNEV000001-T001-P01-F"
-    "ILHNLNEV000002-T001-P01-F"
-    "ILHNLNEV000003-T001-P01-F"
-    "ILHNLNEV000004-T001-P01-F"
-    "ILHNLNEV000005-T001-P01-F"
-    "ILHNLNEV000006-T001-P01-F"
-    "ILHNLNEV000007-T001-P01-F"
-    "ILHNLNEV000008-T001-P01-F"
-    "ILHNLNEV000009-T001-P01-F"
-    "ILHNLNEV000010-T001-P01-F"
-    "ILHNLNEV000011-T001-P01-F"
-    "ILHNLNEV000012-T001-P01-F"
-    "ILHNLNEV000013-T001-P01-F"
-    "ILHNLNEV000014-T001-P01-F"
+    "ILHNLNEV000001"
+    "ILHNLNEV000002"
+    "ILHNLNEV000003"
+    "ILHNLNEV000006"
+    "ILHNLNEV000007"
+    "ILHNLNEV000012"
+    "ILHNLNEV000014"
     )
 
 for seed in ${seeds[@]} 
@@ -27,7 +20,7 @@ do
         while [ $submit_signal == "false" ]
         do
             sleep 30
-            jobs_running=$((`squeue -u psteinberg | wc -l` - 1))
+            jobs_running=$((`squeue -u aneimangolden | wc -l` - 1))
             echo "jobs running: $jobs_running"
             if [ $jobs_running -lt 5 ]
             then
@@ -37,9 +30,9 @@ do
         python3 /hot/software/package/tool-submit-nf/Python/release/2.2.0/submit_nextflow_pipeline.py \
             --nextflow_script /hot/user/yashpatel/pipeline-call-SRC/pipeline-call-SRC/main.nf \
             --nextflow_config /hot/project/method/AlgorithmEvaluation/BNCH-000082-SRCRNDSeed/pipeline-call-src/run-strelka2-battenberg-pyclone-vi/input/config/seed_${seed}.config \
-            --nextflow_yaml /hot/project/method/AlgorithmEvaluation/BNCH-000082-SRCRNDSeed/pipeline-call-src/run-strelka2-battenberg-pyclone-vi/input/yaml/${patient}.yaml \
+            --nextflow_yaml /hot/project/method/AlgorithmEvaluation/BNCH-000082-SRCRNDSeed/pipeline-call-src/run-strelka2-battenberg-pyclone-vi/input/yaml/multi-sample/${patient}.yaml \
             --pipeline_run_name ${patient}_${seed}_Strelka2-Battenberg-PyClone-VI \
             --partition_type F32 \
-            --email psteinberg@mednet.ucla.edu
+            --email aneimangolden@mednet.ucla.edu
     done
 done
