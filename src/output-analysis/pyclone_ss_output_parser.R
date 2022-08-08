bbd1a82e8f9ce3ae12767bc7c3cd56debde23adb
Steinberg <psteinberg@ip-0A12524E.rhxrlfvjyzbupc03cc22jkch3c.xx.internal.cloudapp.net>
2022-08-07 22:20:19 -0700

    Add function to extract ccf and snv per cluster

--- src/output-analysis/pyclone_ss_output_parser.R
+++ src/output-analysis/pyclone_ss_output_parser.R
@@ -10,7 +10,8 @@ input.file <- 'PyClone-VI-0.1.2_%s_%s_Strelka2-Battenberg.tsv'
 # Output directory
 output.dir.stem <- '/hot/project/method/AlgorithmEvaluation/BNCH-000082-SRCRNDSeed/'
 output.pipeline <- 'pipeline-call-src/run-strelka2-battenberg-pyclone-vi/output/'
-output.file <- 'pyclone_ss_subclones_per_patient_seed.tsv'
+output.file.seed <- 'pyclone_ss_subclones_per_patient_seed.tsv'
+output.file.subclone <- 'pyclone_ss_ccf_snv_per_subclones.tsv'
 
 # Pipelines run
 pipeline.runs <- c(
@@ -48,6 +49,7 @@ samples <- c(
     'ILHNLNEV000013-T001-P01-F',
     'ILHNLNEV000014-T001-P01-F'
     );
+
 ### CODE #########################################################################################
 # Get all output paths (Anna)
 all.paths <- list();
@@ -95,8 +97,57 @@ for (paths in all.paths) {
 colnames(all.samples.summary) <- c('patient', 'seed', 'n_clones');
 write.table(
     x = all.samples.summary,
-    file = paste0(output.dir.stem, output.pipeline, Sys.Date(), '_', output.file),
+    file = paste0(output.dir.stem, output.pipeline, Sys.Date(), '_', output.file.seed),
     sep = '\t',
     quote = FALSE,
     row.names = FALSE
     );
+
+### get.snv.ccf ##################################################################################
+# Get number of ccf and snvs per cluster
+get.snvs.per.cluster <- function(table) {
+    cluster.summary <- table %>% group_by(cluster_id) %>% summarise(
+        snv_per_cluster = n(), 
+        ccf_per_cluster = mean(cellular_prevalence)
+        )
+    return(cluster.summary)
+    };
+
+### get.snv.ccf.summary ###########################################################################
+get.snv.ccf.summary <- function(file.path) {
+    table <- read.table(
+        file = file.path,
+        sep = '\t',
+        header = TRUE
+        );
+
+    # get metadata
+    seed <- strsplit(file.path, '_')[[1]][2]
+    sample <- strsplit(file.path, '_')[[1]][3]
+    patient <- strsplit(sample, '-')[[1]][1]
+    
+    # get snv and ccf summary data and add metadata
+    snv.ccf.summary <- get.snvs.per.cluster(table)
+    snv.ccf.summary$seed <- rep(seed, nrow(snv.ccf.summary))
+    snv.ccf.summary$sample <- rep(patient, nrow(snv.ccf.summary))
+    return(snv.ccf.summary)
+    };
+
+### write.snv.ccf.table #######################################################################
+# Add patient vs. seed summary data to summary table
+all.snv.cff.summary <- data.frame();
+    for (paths in all.paths) {
+        for (path in paths) {
+          all.snv.cff.summary <- rbind(all.snv.cff.summary, get.snv.ccf.summary(path))
+        };
+    };
+
+write.table(
+    x = all.snv.cff.summary,
+    file = paste0(output.dir.stem, output.pipeline, Sys.Date(), '_', output.file.subclone),
+    sep = '\t',
+    quote = FALSE,
+    row.names = FALSE
+    );
+
+
